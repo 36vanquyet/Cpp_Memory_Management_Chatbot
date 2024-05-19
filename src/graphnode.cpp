f0236bb19249b3bec7c4801f14240c9fc65740e6
Quyet Cao <36vanquyet@gmail.com>
2024-05-19 13:13:23 +0700

    Task 4 implemented

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -4,6 +4,7 @@
 GraphNode::GraphNode(int id)
 {
     _id = id;
+    _chatBot = nullptr;
 }
 
 GraphNode::~GraphNode()
@@ -11,7 +12,7 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    // delete _chatBot; No need to delete _chatBot here
 
     ////
     //// EOF STUDENT CODE
@@ -27,9 +28,9 @@ void GraphNode::AddEdgeToParentNode(GraphEdge *edge)
     _parentEdges.push_back(edge);
 }
 
-void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
+void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
 {
-    _childEdges.push_back(edge);
+    _childEdges.push_back(std::move(edge));
 }
 
 //// STUDENT CODE
@@ -53,7 +54,7 @@ GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
     //// STUDENT CODE
     ////
 
-    return _childEdges[index];
+    return _childEdges[index].get();
 
     ////
     //// EOF STUDENT CODE
